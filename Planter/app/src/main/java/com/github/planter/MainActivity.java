441688ae1154b648b75740e530978a8b0aebf5a0
dongphusang <marcodsang@gmail.com>
2022-03-29 16:55:28 -0400

    Update_003: GUI transition
    - Added empty activity named "StatBoard"
    - Added translation animation from MainActivity page to StatBoard page

--- Planter/app/src/main/java/com/github/planter/MainActivity.java
+++ Planter/app/src/main/java/com/github/planter/MainActivity.java
@@ -2,13 +2,40 @@ package com.github.planter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // declaring components
+    Button firstPlant;
+    Button secondPlant;
+    Button thirdPlant;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // assigning to views
+        firstPlant = findViewById(R.id.first_plant);
+        secondPlant = findViewById(R.id.second_plant);
+        thirdPlant = findViewById(R.id.third_plant);
+
+        // assigning listener to views
+        firstPlant.setOnClickListener(this);
+        secondPlant.setOnClickListener(this);
+        thirdPlant.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.first_plant)
+        {
+            startActivity(new Intent(getApplicationContext(), StatBoard.class));
+            overridePendingTransition(R.anim.slide_from_right, R.anim.slide_to_left);
+        }
     }
 }
---
+++ Planter/app/src/main/java/com/github/planter/StatBoard.java
@@ -0,0 +1,14 @@
+package com.github.planter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StatBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_board);
+    }
+}
